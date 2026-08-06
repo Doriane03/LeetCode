984073bf300d7fee885997ca55a67f219cb91516
Doriane03 <josephinedorianekouadio@gmail.com>
2026-08-06 12:45:24 +0000

    0577-EMPLOYEE-BONUS Stats: Time: 953 ms (83.78%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT rt.name,rt.bonus
 FROM (SELECT e.name,b.bonus
-FROM Bonus b
-RIGHT JOIN Employee e ON  b.empId=e.empId)rt
+FROM Employee e
+LEFT JOIN Bonus b ON  e.empId=b.empId)rt
 WHERE rt.bonus IS NULL OR rt.bonus<1000;
