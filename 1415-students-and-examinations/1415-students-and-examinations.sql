a812ed655afddaf997d4c66229406da2b9e74abd
Doriane03 <josephinedorianekouadio@gmail.com>
2026-09-17 18:20:11 +0000

    1415-STUDENTS-AND-EXAMINATIONS Stats: Time: 1421 ms (14.01%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -7,6 +7,6 @@ CROSS JOIN Subjects s2
 
 LEFT JOIN Examinations e ON  s1.student_id=e.student_id AND s2.subject_name=e.subject_name 
 
-GROUP BY s1.student_id,s1.student_name,s2.subject_name,e.student_id
+GROUP BY s1.student_id,s1.student_name,s2.subject_name
 
 ORDER BY s1.student_id , s2.subject_name ASC;
